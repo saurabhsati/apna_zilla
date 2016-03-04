6a750fa328ab5005d581a8bfd90f52e240cda9ed
Gayatri Daund <gayatrid@webwingtechnologies.com>
2016-03-04 19:08:03 +0530

    Change in business listing module

--- app/Http/Controllers/Admin/BusinessListingController.php
+++ app/Http/Controllers/Admin/BusinessListingController.php
@@ -26,7 +26,7 @@ class BusinessListingController extends Controller
     {
     	$arr_except_auth_methods = array();
         $this->middleware('\App\Http\Middleware\SentinelCheck',['except' => $arr_except_auth_methods]);
-    	 //$this->UserModel = new UserModel();
+    	  $this->UserModel = new UserModel();
     	  $this->BusinessListingModel = new BusinessListingModel();
     	  $this->RestaurantReviewModel= new RestaurantReviewModel();
           $this->BusinessImageUploadModel=new BusinessImageUploadModel();
@@ -45,12 +45,11 @@ class BusinessListingController extends Controller
     	$business_listing=$this->BusinessListingModel->with(['categoty_details','user_details','reviews'])->get()->toArray();
     	return view('web_admin.business_listing.index',compact('page_title','business_listing','business_public_img_path'));
     }
-
     public function create()
     {
     	$page_title="Create Business List";
 
-    	$obj_user_res = ::where('role','normal')->get();
+    	$obj_user_res = UserModel::where('role','normal')->get();
         if( $obj_user_res != FALSE)
         {
             $arr_user = $obj_user_res->toArray();
@@ -88,7 +87,6 @@ class BusinessListingController extends Controller
 
         return view('web_admin.business_listing.create',compact('page_title','arr_user','arr_category','arr_country','arr_zipcode','arr_city','arr_state'));
     }
-
     public function store(Request $request)
     {
     	$arr_rules	=	array();
@@ -212,7 +210,6 @@ class BusinessListingController extends Controller
         return redirect()->back();
 
     }
-
     public function edit($enc_id)
  	{
  		$id = base64_decode($enc_id);
@@ -267,7 +264,6 @@ class BusinessListingController extends Controller
         return view('web_admin.business_listing.edit',compact('page_title','business_data','arr_user','arr_category','business_public_img_path','business_base_upload_img_path','arr_state','arr_country','arr_zipcode','arr_city','arr_upload_image'));
 
  	}
-
  	public function update(Request $request,$enc_id)
  	{
  		$id	=base64_decode($enc_id);
@@ -384,7 +380,6 @@ class BusinessListingController extends Controller
         }
         return redirect()->back();
    	}
-<<<<<<< HEAD
     public function show($enc_id)
     {
         $id = base64_decode($enc_id);
@@ -396,9 +391,6 @@ class BusinessListingController extends Controller
          return view('web_admin.business_listing.show',compact('page_title','business_data','business_public_img_path','business_base_upload_img_path'));
 
     }
-=======
-
->>>>>>> f26e45d0fd949f53504a4bd3ed5bc7428ab8a905
    	public function toggle_status($enc_id,$action)
     {
         if($action=="activate")
@@ -422,7 +414,6 @@ class BusinessListingController extends Controller
 
         return redirect()->back();
     }
-    
     public function multi_action(Request $request)
     {
         $arr_rules = array();
