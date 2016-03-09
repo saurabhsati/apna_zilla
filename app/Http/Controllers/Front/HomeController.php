d5ad571091017d62dd8986fb69b584b3d8cd4452
Gayatri Daund <gayatrid@webwingtechnologies.com>
2016-03-09 15:36:20 +0530

    Change in Home Controller

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use App\Models\CategoryModel;
 class HomeController extends Controller
 {
     public function __construct()
@@ -15,6 +15,13 @@ class HomeController extends Controller
     }
     public function index()
     {
-    	return view('front.home');
+    	$arr_category = array();
+ 		$obj_category = CategoryModel::where('is_popular','1')->get();
+
+ 		if($obj_category)
+ 		{
+ 			$arr_category = $obj_category->toArray();
+ 		}
+ 		return view('front.home');
     }
 }
