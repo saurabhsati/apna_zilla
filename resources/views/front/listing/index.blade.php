59cb6bf09c09d66fb7f89890abcb260384f34758
Gayatri Daund <gayatrid@webwingtechnologies.com>
2016-04-04 14:56:47 +0530

    Change in business listing send SMS

--- resources/views/front/listing/index.blade.php
+++ resources/views/front/listing/index.blade.php
@@ -209,6 +209,91 @@
         <div class="clr"></div>
       </div>
 
+         <!-- SEND SMS POPUP START-->
+         @if(isset($arr_business) && sizeof($arr_business)>0)
+            @foreach($arr_business as $restaurants)
+            <div class="modal fade" id="sms-{{ $restaurants['id'] }}" role="dialog">
+          <div class="modal-dialog">
+           <!-- Modal content-->
+            <div class="modal-content">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+             <div class="modal-body">
+             <form class="form-horizontal"
+                                    id="validation-form"
+                                    method="POST"
+                                    action="{{ url('/listing/send_sms/') }}"
+                                    enctype="multipart/form-data"
+                                    >
+                {{ csrf_field() }}
+
+
+
+                <b class="head-t">Get information by SMS/Email</b>
+                 <p class="in-li">Enter the details below and click on SEND</p>
+                  <div class="soc-menu-top">
+                      <div class="col-lg-11">
+                  <div class="user_box_sub">
+                                 <div class="row">
+                          <div class="col-lg-3  label-text">Name</div>
+                          <div class="col-sm-12 col-md-12 col-lg-9 m_l">
+                               <input type="text" placeholder="Enter Name"   id="name-{{ $restaurants['id'] }}" name="name-{{ $restaurants['id'] }}" class="input_acct">
+                                <div class="error_msg"></div>
+                              </div>
+                               </div>
+                          </div>
+
+
+
+                  <div class="user_box_sub">
+                                 <div class="row">
+                          <div class="col-lg-3  label-text">Mobile</div>
+                          <div class="col-sm-12 col-md-12 col-lg-9 m_l">
+                              <div class="input-group">
+                              <span id="basic-addon1" class="input-group-addon">+91</span>
+                              <input type="text" required="" aria-describedby="basic-addon1" id="sms_mobile_no-{{ $restaurants['id'] }}" name="sms_mobile_no-{{ $restaurants['id'] }}" placeholder="Mobile" class="form-control">
+
+                              </div>
+                                <div class="error_msg"></div>
+                              </div>
+                               </div>
+                          </div>
+
+
+                      <div class="user_box_sub">
+                                 <div class="row">
+                          <div class="col-lg-3  label-text">Email</div>
+                          <div class="col-sm-12 col-md-12 col-lg-9 m_l">
+                               <input type="text" placeholder="Enter Email" name="email-{{ $restaurants['id'] }}" id="email-{{ $restaurants['id'] }}" class="input_acct">
+                                <div class="error_msg"></div>
+                              </div>
+                               </div>
+                          </div>
+                          <div class="clr"></div>
+                             <div class="user_box_sub">
+                                 <div class="row">
+                          <div class="col-lg-3  label-text">&nbsp;</div>
+                          <div class="col-sm-12 col-md-12 col-lg-9 m_l">
+                          <div class="submit-btn">
+                          <input type="hidden" id="business_id-{{ $restaurants['id'] }}" name="business_id-{{ $restaurants['id'] }}" value="{{ $restaurants['id'] }}">
+                           <button type="button" name="send_sms" id="send_sms" onclick="Send_SMS({{ $restaurants['id'] }})">Send</button>
+                            </div>
+                          </div>
+                                 </div>
+                          </div>
+                      </div>
+                  </div>
+                 <div class="clr"></div>
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+          <!-- SEND SMS POPUP END -->
+         @endforeach
+        @endif
+
+
+
     <!-- location popup end -->
 
     <div id="list_view">
@@ -299,82 +384,7 @@
         </div>
 
 
-<div class="modal fade" id="sms-{{ $restaurants['id'] }}" role="dialog">
-    <div class="modal-dialog">
-     <!-- Modal content-->
-      <div class="modal-content">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-       <div class="modal-body">
-       <form class="form-horizontal"
-                              id="validation-form"
-                              method="POST"
-                              action="{{ url('/listing/send_sms/') }}"
-                              enctype="multipart/form-data"
-                              >
-          {{ csrf_field() }}
 
-
-
-          <b class="head-t">Get information by SMS/Email</b>
-           <p class="in-li">Enter the details below and click on SEND</p>
-            <div class="soc-menu-top">
-                <div class="col-lg-11">
-            <div class="user_box_sub">
-                           <div class="row">
-                    <div class="col-lg-3  label-text">Name</div>
-                    <div class="col-sm-12 col-md-12 col-lg-9 m_l">
-                         <input type="text" placeholder="Enter Name"   id="name-{{ $restaurants['id'] }}" name="name-{{ $restaurants['id'] }}" class="input_acct">
-                          <div class="error_msg"></div>
-                        </div>
-                         </div>
-                    </div>
-
-
-
-            <div class="user_box_sub">
-                           <div class="row">
-                    <div class="col-lg-3  label-text">Mobile</div>
-                    <div class="col-sm-12 col-md-12 col-lg-9 m_l">
-                        <div class="input-group">
-                        <span id="basic-addon1" class="input-group-addon">+91</span>
-                        <input type="text" required="" aria-describedby="basic-addon1" id="sms_mobile_no-{{ $restaurants['id'] }}" name="sms_mobile_no-{{ $restaurants['id'] }}" placeholder="Mobile" class="form-control">
-
-                        </div>
-                          <div class="error_msg"></div>
-                        </div>
-                         </div>
-                    </div>
-
-
-                <div class="user_box_sub">
-                           <div class="row">
-                    <div class="col-lg-3  label-text">Email</div>
-                    <div class="col-sm-12 col-md-12 col-lg-9 m_l">
-                         <input type="text" placeholder="Enter Email" name="email-{{ $restaurants['id'] }}" id="email-{{ $restaurants['id'] }}" class="input_acct">
-                          <div class="error_msg"></div>
-                        </div>
-                         </div>
-                    </div>
-                    <div class="clr"></div>
-                       <div class="user_box_sub">
-                           <div class="row">
-                    <div class="col-lg-3  label-text">&nbsp;</div>
-                    <div class="col-sm-12 col-md-12 col-lg-9 m_l">
-                    <div class="submit-btn">
-                    <input type="hidden" id="business_id-{{ $restaurants['id'] }}" name="business_id-{{ $restaurants['id'] }}" value="{{ $restaurants['id'] }}">
-                     <button type="button" name="send_sms" id="send_sms" onclick="Send_SMS({{ $restaurants['id'] }})">Send</button>
-                      </div>
-                    </div>
-                           </div>
-                    </div>
-                </div>
-            </div>
-           <div class="clr"></div>
-        </div>
-        </form>
-      </div>
-    </div>
-  </div>
 
         @endforeach
         @else
@@ -387,11 +397,8 @@
       <div  id="grid_view" style="display: none;">
           <div class="row">
  @if(isset($arr_business) && sizeof($arr_business)>0)
-
-
-
       @foreach($arr_business as $restaurants)
-       <div class="col-sm-6 col-md-6 col-lg-6">
+  <div class="col-sm-6 col-md-6 col-lg-6">
                          <div class="product_grid_view">
                   <div class="p_images">
                      <div class="grid_product">
@@ -428,16 +435,21 @@
 
                   <div class="p_details"><!--<a href="#" style="border-right:0;display:inline-block;"><i class="fa fa-heart"></i><span> Add to favorites</span></a>-->
                     <ul>
-                    <li><a href="#">SMS/Email</a></li>
+                    <li><a data-toggle="modal" data-target="#sms-{{ $restaurants['id'] }}" href="#">SMS/Email</a></li>
                    <!--  <li><a href="#" class="active">Edit</a></li>
                     <li><a href="#">Own This</a></li> -->
-                    <li><a href="#" class="lst">Rate This</a></li>
+                    <li><a href="{{url('/')}}/{{$city}}/{{$business_area}}/{{base64_encode($restaurants['id'])}}" class="lst">Rate This</a></li>
                     </ul>
                     </div>
                     </div>
 
                 </div>
                 </div>
+
+
+
+
+
 @endforeach
 @else
 <span>No Records Available</span>
