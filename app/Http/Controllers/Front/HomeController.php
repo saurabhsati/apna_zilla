22f09868ac00cc3683e030f46a7fc2606b5ca30e
Gayatri Daund <gayatrid@webwingtechnologies.com>
2016-04-09 10:07:47 +0530

    Changes in home

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -149,10 +149,10 @@ class HomeController extends Controller
 		    $content = file_get_contents($url); // get json content
 
 		    $metadata = json_decode($content, true); //json decoder
-		    if(sizeof($metadata['results'][4]) > 0) {
+		    if(sizeof($metadata['results'][3]) > 0) {
                 //echo '<pre>';
                 //print_r($metadata['results']);exit;
-			    $city = $metadata['results'][4]['address_components']['0']['long_name'];
+			    $city = $metadata['results'][3]['address_components']['1']['long_name'];
                 if(!empty($city))
                 {
 			       Session::put('city', $city);
