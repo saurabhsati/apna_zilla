61eff842d6fd8c29650eb2e03e8dd6ecf9c392bb
gayatri <gayatrid@webwingtechnologie.com>
2016-05-31 15:07:22 +0530

    "changes in business details"

--- resources/views/front/template/_search_top_nav.blade.php
+++ resources/views/front/template/_search_top_nav.blade.php
@@ -121,6 +121,7 @@
               $("input[name='get_city']").val(ui.item.label);
               $("input[name='city_id']").val(ui.item.id);
                setSerchCity(ui.item.id,ui.item.label,view);
+               window.location.reload();
 
             },
             response: function (event, ui)
