6f6ae57d4eae7aa0effa6e550df721f74c887563
Shift <shift@laravelshift.com>
2024-09-27 15:39:44 +0000

    Shift middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,12 +29,14 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\GlobalSiteConfig::class,
         ],
 
         'api' => [
             'throttle:60,1',
+            'bindings',
         ],
         'sentinel_check' => [
             \App\Http\Middleware\SentinelCheck::class,
@@ -50,8 +52,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
